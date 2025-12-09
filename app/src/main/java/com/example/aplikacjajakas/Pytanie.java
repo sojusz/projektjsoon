f13792354df72917f6176e31d852907884a55f10
Wojciech Sojka <127860415+sojusz@users.noreply.github.com>
2025-12-09 11:39:23 +0100

    Update Pytanie.java

--- app/src/main/java/com/example/aplikacjajakas/Pytanie.java
+++ app/src/main/java/com/example/aplikacjajakas/Pytanie.java
@@ -1,32 +1,30 @@
 package com.example.aplikacjajakas;
 
 import com.google.gson.annotations.SerializedName;
+
 public class Pytanie {
     @SerializedName("tresc")
-    private String tresc_pytania;
+    private String trescPytania;
+
     private String odp1;
     private String odp2;
     private String odp3;
-    private Integer poprawnao_odp;
-    private Integer udzielona_odp;
-    private boolean czy_poprawna;
+    private int odp;
 
-    public Pytanie(String tresc_pytania, String odp1, String odp2, String odp3, Integer poprawnao_odp, Integer udzielona_odp, boolean czy_poprawna) {
-        this.tresc_pytania = tresc_pytania;
+    public Pytanie(String trescPytania, String odp1, String odp2, String odp3, int odp) {
+        this.trescPytania = trescPytania;
         this.odp1 = odp1;
         this.odp2 = odp2;
         this.odp3 = odp3;
-        this.poprawnao_odp = poprawnao_odp;
-        this.udzielona_odp = udzielona_odp;
-        this.czy_poprawna = czy_poprawna;
+        this.odp = odp;
     }
 
-    public String getTresc_pytania() {
-        return tresc_pytania;
+    public String getTrescPytania() {
+        return trescPytania;
     }
 
-    public void setTresc_pytania(String tresc_pytania) {
-        this.tresc_pytania = tresc_pytania;
+    public void setTrescPytania(String trescPytania) {
+        this.trescPytania = trescPytania;
     }
 
     public String getOdp1() {
@@ -53,27 +51,11 @@ public class Pytanie {
         this.odp3 = odp3;
     }
 
-    public Integer getPoprawnao_odp() {
-        return poprawnao_odp;
+    public int getOpd() {
+        return odp;
     }
 
-    public void setPoprawnao_odp(Integer poprawnao_odp) {
-        this.poprawnao_odp = poprawnao_odp;
-    }
-
-    public Integer getUdzielona_odp() {
-        return udzielona_odp;
-    }
-
-    public void setUdzielona_odp(Integer udzielona_odp) {
-        this.udzielona_odp = udzielona_odp;
-    }
-
-    public boolean isCzy_poprawna() {
-        return czy_poprawna;
-    }
-
-    public void setCzy_poprawna(boolean czy_poprawna) {
-        this.czy_poprawna = czy_poprawna;
+    public void setOpd(int opd) {
+        this.odp = opd;
     }
 }
